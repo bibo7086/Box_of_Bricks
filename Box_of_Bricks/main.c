b78ab8a530ce25153ef7381b8e564b34f8631a94
Saeed Aliyu <saeedaliyu@Saeeds-MacBook-Pro.local>
2015-07-24 21:58:38 +0100

    Initial Commit

---
+++ Box_of_Bricks/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Box_of_Bricks
+//
+//  Created by Saeed Aliyu on 7/24/15.
+//  Copyright (c) 2015 Saeed Aliyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
